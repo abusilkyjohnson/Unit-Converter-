dc524ede86bdd9cb7101b08c064b7503a77ca992
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-04 23:03:45 -0500

    functionality and background added

--- app/src/main/java/com/example/unitconversionlbstostone/MainActivity.java
+++ app/src/main/java/com/example/unitconversionlbstostone/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.unitconversionlbstostone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText inputWeight = findViewById(R.id.inputweightXML);
+        TextView resultView = findViewById(R.id.resultView);
+
+
+
+
+        Button myBtn = findViewById(R.id.convertButt);
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userWeightInput = inputWeight.getText().toString();// be careful where this is created because on the onCreate and OnClick Methods
+                float weight = Float.parseFloat(userWeightInput);
+                float result = weight/14;
+                String resultString = String.valueOf(result);
+
+                resultView.setText("" + result);
+            }
+        });
+
     }
 }
